9e6b09ce5d133ddbd81c71567b274a4df9a31743
Kristian laskemoen <Kristian@laskemoen.com>
2013-06-17 21:33:08 +0200

    first task

---
+++ Kabal/src/kabal/Game.java
@@ -0,0 +1,13 @@
+package kabal;
+
+public class Game {
+	public static void main(String[] args){
+		new Game();
+	}
+	
+	//Game object
+	public Game(){
+		System.out.println("new Game");
+		System.out.println(new Card());
+	}
+}
